038a4c114b6a4df217ae4bb45bf06e7f94cfa5ae
Calvin Dong <calvind777@gmail.com>
2016-07-16 13:08:23 -0700

    map

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 function showMap() {
   console.log(myLocLatLng);
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 13,
     center: {lat: myLocLatLng[0], lng: myLocLatLng[1]}
   });
   $("#container").toggle();
