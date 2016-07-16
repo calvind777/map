cf4e432a6578e5a4d904a56f6fadead9ae24e9a8
Calvin Dong <calvind777@gmail.com>
2016-07-16 11:11:17 -0700

    asf

--- js/viewer.js
+++ js/viewer.js
@@ -310,7 +310,7 @@ function vector3ToLatLong(v, radius) {
     var lat = phi*180/Math.PI;
     var lon = theta*180/Math.PI + 180;
     
-    return new {'lat':lat,'lng':lon};
+    return  {'lat':lat,'lng':lon};
 }
 
 function animate() {
