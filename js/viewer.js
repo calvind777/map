81dfcf0c126f00fc67057f10e36801fd12a858e3
Michael Ge <michael.zh.ge@gmail.com>
2016-07-16 10:54:44 -0700

    awef

--- js/viewer.js
+++ js/viewer.js
@@ -129,7 +129,7 @@ function addToGroup() {
 var isClicked = false;
 function disturb(e, click) {
     mouse.x = e.clientX / WIDTH * 2 - 1;
-    mouse.y = e.clientY / HEIGHT * -2 + 1 + OFFSET;
+    mouse.y = e.clientY / HEIGHT * -2 + 1;
     raycaster.setFromCamera(mouse, camera);
 
     var intersects = raycaster.intersectObjects(balls);
