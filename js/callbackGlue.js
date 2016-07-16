7feee2b6e042e8a6c64ab6ec4c926bd32d9f14a8
Jimmy Ye <jiy162@ucsd.edu>
2016-07-16 05:23:56 -0700

    Attempt to not exceed geocoding rate limit

--- js/callbackGlue.js
+++ js/callbackGlue.js
@@ -2,16 +2,20 @@ var nameLocationWork = [];
 var myLocLatLng;
 var pointPairs = [];
 nameLocationWork.push = function() {
+    if (!myLocLatLng && myLocation) {
+        geocodeAddress(geocoder, myLocation, function(success, location) {
+            myLocLatLng = [location.lat(), location.lng()];
+        });
+        console.log("Got myLocation");
+    }
     geocodeAddress(geocoder, arguments[0].location, function(success, location) {
         if(success){
-            if (!myLocLatLng && myLocation) {
-                geocodeAddress(geocoder, myLocation, function(success, location) {
-                    myLocLatLng = [location.lat(), location.lng()];
-                });
-            }
             pointPairs.push([myLocLatLng,[location.lat(), location.lng()]]);
             setData(pointPairs, false);
             console.log("Added " + arguments[0].location);
+        } else {
+            setTimeout(function() {}, 500)
+            return nameLocationWork.push(arguments[0]);
         }
     });
     return Array.prototype.push.apply(this, arguments);
