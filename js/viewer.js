d763948e62b161c1df0caf9c725be19499e2dda8
Calvin Dong <calvind777@gmail.com>
2016-07-16 10:43:29 -0700

    fix

--- js/viewer.js
+++ js/viewer.js
@@ -156,8 +156,8 @@ function disturb(e, click) {
         mesh.material.color = y;
         var vector3 = mesh.position;
         var ltln = vector3ToLatLong(vector3, 0.5);
-        for (var a = 0; a < nameLocationWork.length;a++) {
-            if (nameLocationWork[a].locationCoords.lat = ltln.lat && nameLocationWork[a].locationCoords.lng = ltln.lng) {
+        for (var a = 0; a < nameLocationWork.length ;a++) {
+            if (nameLocationWork[a].locationCoords.lat == ltln.lat && nameLocationWork[a].locationCoords.lng == ltln.lng) {
                 clickedCity = nameLocationWork[a].substring(0,nameLocationWork[a].location.indexOf(','));
                 alert(clickedCity);
             }
